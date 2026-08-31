c3d91e8b61527dab57e2e484772415e9fe2775bd
Niklas <nizc@itu.dk>
2026-08-31 15:25:15 +0200

    create initial project and gitignore
    Co-authored-by: David <davidfeldner>

---
+++ src/test/java/dk/itu/swandb/AppTest.java
@@ -0,0 +1,12 @@
+package dk.itu.swandb;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+    @Test
+    void testApp() {
+        assertTrue(true);
+    }
+}
