b1c5d32e29dcee75b1f039b1b9a779a942812b06
Niklas <nizc@itu.dk>
2026-08-31 15:38:04 +0200

    feat: setup backend configuration and create first test
    Co-authored-by: David <davidfeldner>

---
+++ src/test/java/dk/itu/swandb/EngineTest.java
@@ -0,0 +1,11 @@
+package dk.itu.swandb;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class EngineTest {
+    @Test
+    void teamName() {
+        assertEquals("Team swandb", new Engine().teamName());
+    }
+}
